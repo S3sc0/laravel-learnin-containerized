bb3a7c7a3cba735769961741325b4c03ea09e6c6
Ayoub Amzouar <aamzouar@student.1337.ma>
2023-09-16 00:43:31 +0100

    refactor: remove sudo command docker now works without root permission

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ "$1" == "-c" ]; then
-	sudo docker build -t laravel ./srcs
-	sudo docker run -d -it -v $(pwd)/srcs/autolearn:/root/autolearn -p8000:8000 --name laravel-cntr laravel
+	docker build -t laravel ./srcs
+	docker run -d -it -v $(pwd)/srcs/autolearn:/root/autolearn -p8000:8000 --name laravel-cntr laravel
 
 elif [ "$1" == "-d" ]; then
-	sudo docker stop laravel-cntr > /dev/null
-	sudo docker rm laravel-cntr > /dev/null
+	docker stop laravel-cntr > /dev/null
+	docker rm laravel-cntr > /dev/null
 
-	sudo docker image rm -f laravel	> /dev/null
+	docker image rm -f laravel	> /dev/null
 
 else
 	printf "Usage:\t./run.sh [OPTIONS]
