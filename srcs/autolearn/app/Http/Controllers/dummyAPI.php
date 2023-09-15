2c1274488a54284dd48c64aa0b4e9d0ac6218d42
Ayoub Amzouar <aamzouar@student.1337.ma>
2023-09-16 00:51:09 +0100

    dummy apis

---
+++ srcs/autolearn/app/Http/Controllers/dummyAPI.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dummyAPI extends Controller
+{
+    function	getData() {
+		return ["full name"=>"sesco 9adir", "secret"=>"f u", "more"=>"9ra o zid 9ra"];	
+	}
+}
